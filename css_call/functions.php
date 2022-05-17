4652704cdd2ce20bae3962f0aa80da8a7fd16d72
hasancoders0 <105715250+hasancoders0@users.noreply.github.com>
2022-05-17 23:42:21 +0600

    Update functions.php

--- css_call/functions.php
+++ css_call/functions.php
@@ -1,1 +1,16 @@
+//the code put in functions.php
 
+function sundarban_js_css_file(){
+    //default css calling
+    wp_enqueue_style( 'sundarban_style', get_stylesheet_uri());
+    //custom css calling
+    wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.0.2' );
+    wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0' ,'all');
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_style('custom');
+    //custom js calling
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.0.2', 'true');
+    wp_enqueue_script( 'main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true');
+}
+add_action( 'wp_enqueue_scripts','sundarban_js_css_file' );
