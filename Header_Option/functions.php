c8792edd8415133cf3fdc5c7ea273c73e912e629
hasancoders0 <105715250+hasancoders0@users.noreply.github.com>
2022-05-18 00:11:31 +0600

    Update functions.php

--- Header_Option/functions.php
+++ Header_Option/functions.php
@@ -1,1 +1,27 @@
+//Theme Function\Header area
+function sundarban_header_area_customize($wp_customize){
+    $wp_customize->add_section('sundarban_header_area', array(
+        'title'=>__('Header Area', 'sundarban'),
+        'description' => 'Please Update Your Site Logo Here.'
+    ));
+
+    $wp_customize->add_setting('sundarban_theme_logo', array(
+        'default' => get_bloginfo('template_directory'). '/img/logo.png',
+    ));
+    
+
+    
+    $wp_customize-> add_control(new WP_Customize_Image_Control($wp_customize, 'sundarban_theme_logo',array(
+        'lebel' => 'Logo Upload',
+        'discription' => 'Upload your own Logo',
+        'setting' => 'sundarban_theme_logo',
+        'section' => 'sundarban_header_area',
+    )));
+
+  
+}
+add_action( 'customize_register','sundarban_header_area_customize' );
+
+register_nav_menu('main_menu',__('Main Menu','sundarban' ));
+
 
