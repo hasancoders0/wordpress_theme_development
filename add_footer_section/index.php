a76df80ae1d9b7484de84259ad7a7f57c8383394
hasancoders0 <105715250+hasancoders0@users.noreply.github.com>
2022-05-21 23:44:23 +0600

    Create index.php

---
+++ add_footer_section/index.php
@@ -0,0 +1,13 @@
+<footer id="footer_area">
+
+
+    <section id="copyright_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p><?php echo get_theme_mod( 'sundarban_footer_copyright_section'); ?></p>
+                </div>
+            </div>
+        </div>
+    </section>
+</footer>
