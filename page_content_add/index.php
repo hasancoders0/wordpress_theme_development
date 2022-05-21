68b4cc7b4f23724852d3f9842145efd7cc65c8f9
hasancoders0 <105715250+hasancoders0@users.noreply.github.com>
2022-05-21 20:42:15 +0600

    Create index.php

---
+++ page_content_add/index.php
@@ -0,0 +1,9 @@
+<section id="body_area">
+    <div class="">
+        <div class="">
+            <div class="">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+</section>
