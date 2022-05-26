45414bd8090491cb49f85cc6c9b0c744996307bb
hasancoders0 <105715250+hasancoders0@users.noreply.github.com>
2022-05-26 22:44:17 +0600

    Create default.php

---
+++ Theme_slicing/inc/default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file for default function
+ */
+
+//  Theme Title
+
+	
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'responsive-embeds' );
+add_theme_support( 'editor-styles' );
+add_theme_support( 'html5', array('style','script', ) );
+add_theme_support( 'automatic-feed-links' );
