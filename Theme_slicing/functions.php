841b3ec530977ae8d7df93d7c113a05249f12b6c
hasancoders0 <105715250+hasancoders0@users.noreply.github.com>
2022-05-26 22:42:24 +0600

    Create functions.php

---
+++ Theme_slicing/functions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * My theme function
+ */
+
+
+ //this is default function php file calling
+include_once('inc/default.php');
+
+
+//enqueue php file calling
+include_once('inc/enqueue.php');
+
+
+
+//Theme Function\Header area\footer area
+include_once('inc/hf_function.php');
+
+
+
+//Menu function calling
+include_once('inc/menu_function.php');
+
+
+
+ 
+
