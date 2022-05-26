c31522c00a506fb13227fe273e8c0bddac50fafc
hasancoders0 <105715250+hasancoders0@users.noreply.github.com>
2022-05-26 22:39:14 +0600

    Create 404.php

---
+++ Theme_slicing/404.php
@@ -0,0 +1,15 @@
+<!-- header calling -->
+<?php get_header() ?>
+
+<section id="body_area">
+    <div class="">
+        <div class="">
+            <div class="">
+                <p>This page is not avalavle</p>
+            </div>
+        </div>
+    </div>
+</section>
+  
+<!-- Calling Footer -->
+<?php get_footer(  ) ?>
