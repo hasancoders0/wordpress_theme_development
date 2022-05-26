399d2d9cdd54c4b796e6399429f6191d8370233d
hasancoders0 <105715250+hasancoders0@users.noreply.github.com>
2022-05-26 22:39:40 +0600

    Create author.php

---
+++ Theme_slicing/author.php
@@ -0,0 +1,15 @@
+<!-- header calling -->
+<?php get_header() ?>
+
+<section id="body_area">
+    <div class="">
+        <div class="">
+            <div>
+                <p></p>
+            </div>
+        </div>
+    </div>
+</section>
+  
+<!-- Calling Footer -->
+<?php get_footer(  ) ?>
