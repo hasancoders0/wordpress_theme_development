808fae3b47e0ee55b1434170bf4499c2a82ca1f5
hasancoders0 <105715250+hasancoders0@users.noreply.github.com>
2022-05-30 21:58:58 +0600

    Create index.php

---
+++ Pagination_set/index.php
@@ -0,0 +1,6 @@
+<div id="page_nav">
+            <?php if ('sundarban_pagenav') {sundarban_pagenav(); } else{ ?>
+                <?php next_posts_link(); ?>
+                <?php previous_posts_link(); ?>
+            <?php } ?>
+          </div>
